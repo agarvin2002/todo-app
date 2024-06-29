a3b5276dcb7a0569b4d4719595680e55c785e66e
agarvin2002 <agarvin2002@gmail.com>
2024-06-29 15:14:50 +0530

    aded validation

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,27 @@ const {createTodo, updateTodo} = require("./types");
 app.use(express.json);
 
 app.post("/todo", (req, res) => {
-
+    const createPayload = req.body;
+    const parsedPayoad = createTodo.safeParse(createPayload);
+    if(!parsedPayoad.success){
+        res.status(411).json({
+            msg : "You sent the wront inputs",
+        })
+        return;
+    }
 });
 app.get("/todo", (req, res) => {
 
 });
 app.put("/completed", (req, res) => {
-
+    const updatePayload = req.body;
+    const parsedPayoad = updateTodo.safeParse(updatePayload);
+    if(!parsedPayoad.success){
+        res.status(411).json({
+            msg : "You sent the wront inputs",
+        })
+        return;
+    }
 });
 
 app.listen(3000, console.log("Server listening on port 3000"));
